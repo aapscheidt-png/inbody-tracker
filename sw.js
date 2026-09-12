906d78fa7093f7389584315c82bb58a0cb1ae1a2
Anderson Pscheidt <aapscheidt@gmail.com>
2026-09-12 17:09:26 -0300

    V4.1: refresh PWA cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='health-tracker-v4';
+const CACHE='health-tracker-v4-1';
 const ASSETS=['./','./index.html','./styles.css','./core.js','./charts.js','./render.js','./ui.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
