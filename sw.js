f13a03e2d4e87ec19e18a354cd856736cae49dbf
Anderson Pscheidt <aapscheidt@gmail.com>
2026-09-12 15:17:40 -0300

    V3: refresh PWA cache for health modules

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='inbody-tracker-v2';
-const ASSETS=['./','./index.html','./styles.css','./core.js','./charts.js','./render.js','./ui.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='health-tracker-v3';
+const ASSETS=['./','./index.html','./styles.css','./v3.css','./core-v3.js','./charts-v3.js','./render-v3.js','./ui-v3.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html')))));
