2363a61d1ed3e8c371714fc73722849e064c0a96
Anderson Pscheidt <aapscheidt@gmail.com>
2026-09-12 13:01:40 -0300

    Privacy V2: refresh offline cache manifest

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='inbody-tracker-v1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='inbody-tracker-v2';
+const ASSETS=['./','./index.html','./styles.css','./core.js','./charts.js','./render.js','./ui.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html')))));
